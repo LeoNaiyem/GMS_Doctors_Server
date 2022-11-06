eeb35c85d09b1006f90461b0ada0587e14b01d37
KawserAlam21 <mdnaiyem.islam.10@gmail.com>
2022-11-06 20:10:54 +0600

    added payment routes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const ObjectId = require("mongodb").ObjectId;
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 //MIDDLEWARE
@@ -22,6 +23,7 @@ async function run() {
     const database = client.db("GMS_Doctors");
     const serviceCollection = database.collection("services");
     const appointmentCollection = database.collection("appointments");
+    const paymentCollection = database.collection("payments");
 
     // loading data from the database
     app.get("/services", async (req, res) => {
@@ -30,10 +32,33 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+    app.get("/services/names", async (req, res) => {
+      const servicesName = await serviceCollection
+        .find()
+        .project({ serviceName: 1, _id: 0 })
+        .toArray();
+      res.send(servicesName);
+    });
+
+    app.get("/services/:service", async (req, res) => {
+      const serviceName = req.params.service;
+      const query = { serviceName: serviceName };
+      const result = await serviceCollection.findOne(query);
+      res.send(result);
+    });
+
     app.get("/appointments", async (req, res) => {
-      const query = {};
+      const email = req.query.email;
+      const query = { email: email };
       const cursor = appointmentCollection.find(query);
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/appointments/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await appointmentCollection.findOne(query);
       res.send(result);
     });
 
@@ -46,11 +71,9 @@ async function run() {
 
     app.post("/appointments", async (req, res) => {
       const appointment = req.body;
-      console.log(appointment);
       const result = await appointmentCollection.insertOne(appointment);
       res.send(result);
     });
-
 
     //deleting data from the server
     app.delete("/services/:id", async (req, res) => {
@@ -66,6 +89,40 @@ async function run() {
       const result = await appointmentCollection.deleteOne(query);
       res.send(result);
     });
+
+    //payment
+    app.post("/create-payment-intent", async (req, res) => {
+      const appointment = req.body;
+      const price = appointment.price;
+      const amount = price *100;
+
+      // Create a PaymentIntent with the order amount and currency
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "USD",
+        payment_method_types: ["card"],
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+
+    app.patch("/appointments/:id", async (req, res) => {
+      const id = req.params.id;
+      const payment = req.body;
+      const filter = { _id: ObjectId(id) };
+      const updatedDoc = {
+        $set: { paid: true, transactionId: payment.transactionId },
+      };
+      const updatedResult = await appointmentCollection.updateOne(
+        filter,
+        updatedDoc
+      );
+      const result = await paymentCollection.insertOne(payment);
+      res.send(updatedResult);
+    });
+
   } finally {
     // await client.close();
   }
